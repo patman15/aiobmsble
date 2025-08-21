096f003a9869cc10e718c09fc5018b3b113ff4cf
patman15 <14628713+patman15@users.noreply.github.com>
2025-08-21 19:30:58 +0200

    restructure code

--- aiobmsble/__main__.py
+++ aiobmsble/__main__.py
@@ -16,11 +16,6 @@ from bleak.exc import BleakError
 from aiobmsble import BMSsample
 from aiobmsble.utils import bms_supported
 
-BMS_PLUGINS: Final[set[ModuleType]] = {
-    importlib.import_module(f"aiobmsble.bms.{module_name}")
-    for _, module_name, _ in pkgutil.iter_modules(["aiobmsble/bms"])
-}
-
 logging.basicConfig(
     format="%(levelname)s: %(message)s",
     level=logging.INFO,
@@ -28,15 +23,33 @@ logging.basicConfig(
 logger: logging.Logger = logging.getLogger(__name__)
 
 
-async def detect_bms(loglevel: int) -> None:
-    """Query a Bluetooth device based on the provided arguments."""
+def load_bms_plugins() -> set[ModuleType]:
+    """Load all available BMS plugin modules."""
+    bms_plugins: set[ModuleType] = {
+        importlib.import_module(f"aiobmsble.bms.{module_name}")
+        for _, module_name, _ in pkgutil.iter_modules(["aiobmsble/bms"])
+    }
+    logger.debug(
+        "loaded BMS types: %s", [key.__name__.rsplit(".", 1)[-1] for key in bms_plugins]
+    )
+    return bms_plugins
 
+
+async def scan_devices() -> dict[str, tuple[BLEDevice, AdvertisementData]]:
+    """Scan for BLE devices and return results."""
     logger.info("starting scan...")
     scan_result: dict[str, tuple[BLEDevice, AdvertisementData]] = (
         await BleakScanner.discover(return_adv=True)
     )
     logger.info("%i BT devices in range.", len(scan_result))
+    return scan_result
 
+
+async def detect_bms(loglevel: int) -> None:
+    """Query a Bluetooth device based on the provided arguments."""
+
+    bms_plugins: set[ModuleType] = load_bms_plugins()
+    scan_result: dict[str, tuple[BLEDevice, AdvertisementData]] = await scan_devices()
     for ble_dev, advertisement in scan_result.values():
         logger.info(
             "%s\nBT device '%s' (%s)\n\t%s",
@@ -45,25 +58,43 @@ async def detect_bms(loglevel: int) -> None:
             ble_dev.address,
             repr(advertisement).replace(", ", ",\n\t"),
         )
-        for bms_module in BMS_PLUGINS:
-            if bms_supported(bms_module.BMS, advertisement):
-                logger.info(
-                    "Found matching BMS type: %s",
-                    bms_module.__name__.rsplit(".", maxsplit=1)[-1],
-                )
-                bms: Any = bms_module.BMS(ble_device=ble_dev, reconnect=True)
-                logging.getLogger(
-                    f"{bms_module.__name__.replace('.bms', '')}::{ble_dev.name}:"
-                    f"{ble_dev.address[-5:].replace(':','')}"
-                ).setLevel(loglevel)
-                try:
-                    logger.info("Updating BMS data...")
-                    data: BMSsample = await bms.async_update()
-                    logger.info("BMS data: %s", repr(data).replace(", ", ",\n\t"))
-                except (BleakError, TimeoutError) as ex:
-                    logger.error("Failed to update BMS: %s", ex)
+        for bms_module in bms_plugins:
+            if not bms_supported(bms_module.BMS, advertisement):
+                continue
+
+            logger.info(
+                "Found matching BMS type: %s",
+                bms_module.__name__.rsplit(".", maxsplit=1)[-1],
+            )
+            bms: Any = bms_module.BMS(ble_device=ble_dev, reconnect=True)
+            logging.getLogger(
+                f"{bms_module.__name__.replace('.bms', '')}::{ble_dev.name}:"
+                f"{ble_dev.address[-5:].replace(':','')}"
+            ).setLevel(loglevel)
+            try:
+                logger.info("Updating BMS data...")
+                data: BMSsample = await bms.async_update()
+                logger.info("BMS data: %s", repr(data).replace(", ", ",\n\t"))
+            except (BleakError, TimeoutError) as ex:
+                logger.error("Failed to update BMS: %s", ex)
 
     logger.info("done.")
+
+
+def setup_logging(args: argparse.Namespace) -> int:
+    """Configure logging based on command line arguments."""
+    loglevel: Final[int] = logging.DEBUG if args.verbose else logging.INFO
+
+    if args.logfile:
+        file_handler = logging.FileHandler(args.logfile)
+        file_handler.setLevel(loglevel)
+        file_handler.setFormatter(
+            logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
+        )
+        logger.addHandler(file_handler)
+
+    logger.setLevel(loglevel)
+    return loglevel
 
 
 def main() -> None:
@@ -76,21 +107,7 @@ def main() -> None:
         "-v", "--verbose", action="store_true", help="Enable debug logging"
     )
 
-    args: argparse.Namespace = parser.parse_args()
-    loglevel: Final[int] = logging.DEBUG if args.verbose else logging.INFO
-    if args.logfile:
-        file_handler = logging.FileHandler(args.logfile)
-        file_handler.setLevel(loglevel)
-        file_handler.setFormatter(
-            logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
-        )
-        logger.addHandler(file_handler)
-
-    logger.setLevel(loglevel)
-
-    logger.debug(
-        "loaded BMS types: %s", [key.__name__.rsplit(".", 1)[-1] for key in BMS_PLUGINS]
-    )
+    loglevel: Final[int] = setup_logging(parser.parse_args())
 
     asyncio.run(detect_bms(loglevel))
 
