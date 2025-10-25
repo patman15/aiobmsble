52d2c6c8328052d6b2927c444ac16b80fee6109c
patman15 <14628713+patman15@users.noreply.github.com>
2025-10-25 19:25:02 +0200

    add device info readout

--- aiobmsble/bms/pace_bms.py
+++ aiobmsble/bms/pace_bms.py
@@ -12,7 +12,7 @@ from bleak.backends.device import BLEDevice
 from bleak.uuids import normalize_uuid_str
 
 from aiobmsble import BMSDp, BMSInfo, BMSSample, BMSValue, MatcherPattern
-from aiobmsble.basebms import BaseBMS, crc_modbus
+from aiobmsble.basebms import BaseBMS, barr2str, crc_modbus
 
 
 class BMS(BaseBMS):
@@ -24,12 +24,12 @@ class BMS(BaseBMS):
     }  # TODO: fill correct manufacturer/model
     _HEAD: Final[bytes] = b"\x9a"  # beginning of frame
     _TAIL: Final[bytes] = b"\x9d"  # end of frame
-    # _FRAME_LEN: Final[int] = 10  # length of frame, including SOF and checksum
+    _MIN_LEN: Final[int] = 11  # minimal frame length
     _FIELDS: Final[tuple[BMSDp, ...]] = (
-        BMSDp("current", 1, 4, True, lambda x: x/100),
-        BMSDp("voltage", 5, 4, False, lambda x: x/100),
-        BMSDp("cycle_charge", 9, 4, False, lambda x: x/100),
-        BMSDp("design_capacity", 13, 4, False, lambda x: x//100),
+        BMSDp("current", 1, 4, True, lambda x: x / 100),
+        BMSDp("voltage", 5, 4, False, lambda x: x / 100),
+        BMSDp("cycle_charge", 9, 4, False, lambda x: x / 100),
+        BMSDp("design_capacity", 13, 4, False, lambda x: x // 100),
         BMSDp("battery_level", 21, 1, False, lambda x: x),
         # BMSDp("battery_health", 22, 1, False, lambda x: x),
         BMSDp("pack_count", 0, 1, False, lambda x: x),
@@ -63,7 +63,14 @@ class BMS(BaseBMS):
 
     async def _fetch_device_info(self) -> BMSInfo:
         """Fetch the device information via BLE."""
-        raise NotImplementedError  # TODO: implement code or delete to query service 0x180A
+        result: BMSInfo = BMSInfo()
+        await self._await_reply(self._cmd(b"\x00\x00\x00\x02\x00\x00"))
+        length: int = self._data[8]
+        result["serial_number"] = barr2str(self._data[9 : 9 + length])
+        await self._await_reply(self._cmd(b"\x00\x00\x00\x01\x00\x00"))
+        result["sw_version"] = barr2str(self._data[10:10+self._data[9]])
+        result["hw_version"] = barr2str(self._data[65:65+self._data[64]])
+        return result
 
     @staticmethod
     def _calc_values() -> frozenset[BMSValue]:
@@ -79,6 +86,10 @@ class BMS(BaseBMS):
 
         if not data.startswith(BMS._HEAD):
             self._log.debug("incorrect SOF")
+            return
+
+        if len(data) < BMS._MIN_LEN or len(data) != BMS._MIN_LEN + data[7]:
+            self._log.debug("incorrect frame length")
             return
 
         if (crc := crc_modbus(data[:-3])) != int.from_bytes(
