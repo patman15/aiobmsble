5bd1d88bcb5c2fdd8ee983cedab63d30de30ac17
Patrick <14628713+patman15@users.noreply.github.com>
2025-12-21 15:33:53 +0100

    remove test data dependency
    avoid generate_advertisement_data import

--- tests/bluetooth.py
+++ tests/bluetooth.py
@@ -7,25 +7,6 @@ Source: /tests/components/bluetooth/__init__.py
 from typing import Any
 
 from bleak.backends.device import BLEDevice
-from bleak.backends.scanner import AdvertisementData
-
-ADVERTISEMENT_DATA_DEFAULTS = {
-    "local_name": "",
-    "manufacturer_data": {},
-    "service_data": {},
-    "service_uuids": [],
-    "rssi": -127,
-    "platform_data": ((),),
-    "tx_power": -127,
-}
-
-
-def generate_advertisement_data(**kwargs: Any) -> AdvertisementData:
-    """Generate advertisement data with defaults."""
-    new: dict[str, Any] = kwargs.copy()
-    for key, value in ADVERTISEMENT_DATA_DEFAULTS.items():
-        new.setdefault(key, value)
-    return AdvertisementData(**new)
 
 
 def generate_ble_device(
