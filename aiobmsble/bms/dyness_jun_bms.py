e0081ced7e72246a333a36efd5bb399096c3d4df
Patrick <14628713+patman15@users.noreply.github.com>
2026-03-18 23:21:15 +0100

    add init command

--- aiobmsble/bms/dyness_jun_bms.py
+++ aiobmsble/bms/dyness_jun_bms.py
@@ -4,6 +4,9 @@ Project: aiobmsble, https://pypi.org/p/aiobmsble/
 License: Apache-2.0, http://www.apache.org/licenses/
 """
 
+from typing import Final
+
+from bleak.backends.characteristic import BleakGATTCharacteristic
 from bleak.uuids import normalize_uuid_str
 
 from aiobmsble import BMSInfo, MatcherPattern
@@ -17,6 +20,7 @@ class BMS(JbdBMS):
         "default_manufacturer": "Dyness",
         "default_model": "Junior BMS",
     }
+    _INIT_CMD: Final[bytes] = b"\xff\xaa\x55\x00\x00\x77"  # initialization command
 
     @staticmethod
     def matcher_dict_list() -> list[MatcherPattern]:
@@ -37,3 +41,9 @@ class BMS(JbdBMS):
     def uuid_tx() -> str:
         """Return 16-bit UUID of characteristic that provides write property."""
         return "fe01"
+
+    async def _init_connection(
+        self, char_notify: BleakGATTCharacteristic | int | str | None = None
+    ) -> None:
+        await self._await_msg(BMS._INIT_CMD, normalize_uuid_str("fed5"), wait_for_notify=False)
+        await super()._init_connection(char_notify)
