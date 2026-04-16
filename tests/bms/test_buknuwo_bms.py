a477e2643c9aa431a02865c99023128665faaab9
Patrick <14628713+patman15@users.noreply.github.com>
2026-04-16 09:51:50 +0200

    complete test coverage

--- tests/bms/test_buknuwo_bms.py
+++ tests/bms/test_buknuwo_bms.py
@@ -5,7 +5,9 @@ from typing import Final
 from uuid import UUID
 
 from bleak.backends.characteristic import BleakGATTCharacteristic
+import pytest
 
+from aiobmsble import BMSSample
 from aiobmsble.bms.buknuwo_bms import BMS
 from tests.bluetooth import generate_ble_device
 from tests.conftest import MockBleakClient
@@ -23,7 +25,7 @@ class TestBasicBMS(BMSBasicTests):
 class MockBuknuwoBleakClient(MockBleakClient):
     """Emulate a Buknuwo BMS BleakClient."""
 
-    RESP: dict[bytes, bytearray] = {
+    _RESP: dict[bytes, bytearray] = {
         b"\x01\x03\x00\x0b\x00\x01\xf5\xc8": bytearray(
             b"\x01\x03\x02\x0c\x00\xbd\x44"
         ),  #  info
@@ -73,7 +75,7 @@ class MockBuknuwoBleakClient(MockBleakClient):
         if char_specifier != "00002760-08c2-11e1-9073-0e8ac72e0001":
             return  # only respond to writes to TX characteristic
 
-        _response: Final[bytearray] = self.RESP.get(bytes(data), bytearray())
+        _response: Final[bytearray] = self._RESP.get(bytes(data), bytearray())
         for notify_data in [
             _response[i : i + BT_FRAME_SIZE]
             for i in range(0, len(_response), BT_FRAME_SIZE)
@@ -118,3 +120,48 @@ async def test_update(patch_bleak_client, keep_alive_fixture: bool) -> None:
 async def test_device_info(patch_bleak_client) -> None:
     """Test that the BMS returns initialized dynamic device information."""
     await verify_device_info(patch_bleak_client, MockBleakClient, BMS)
+
+
+@pytest.mark.parametrize(
+    ("wrong_response"),
+    [
+        b"",
+        b"\x01\x03",
+        b"\x01\x04\x1a\x01\x84\x05\x59\x00\x63\x00\x64\x20\x56\x20\xa1\x23\x28\x00\x01\x00\x00\x00"
+        b"\x04\x80\x00\x2d\x00\x00\x00\x2d\xdb",
+        b"\x01\x03\x1a\x01\x84\x05\x59\x00\x63\x00\x64\x20\x56\x20\xa1\x23\x28\x00\x01\x00\x00\x00"
+        b"\x04\x80\x00\x2d\x00\x00\x00\xff\xff",
+        b"\x01\x03\x0f\x60\xf4",
+    ],
+    ids=[
+        "empty",
+        "minimal",
+        "wrong_SOF",
+        "wrong_CRC",
+        "wrong_length",
+    ],
+)
+async def test_invalid_response(
+    monkeypatch: pytest.MonkeyPatch,
+    patch_bleak_client,
+    patch_bms_timeout,
+    wrong_response: bytes,
+) -> None:
+    """Test data up date with BMS returning invalid data."""
+
+    patch_bms_timeout()
+    monkeypatch.setattr(
+        MockBuknuwoBleakClient,
+        "_RESP",
+        {b"\x01\x03\x00\x00\x00\x0d\x84\x0f": bytearray(wrong_response)},
+    )
+    patch_bleak_client(MockBuknuwoBleakClient)
+
+    bms = BMS(generate_ble_device())
+
+    result: BMSSample = {}
+    with pytest.raises(TimeoutError):
+        result = await bms.async_update()
+
+    assert not result
+    await bms.disconnect()
