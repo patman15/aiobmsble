3b89de1408031a807e3fc6c58eba4b1e08b52d25
Patrick <14628713+patman15@users.noreply.github.com>
2026-04-30 20:19:32 +0200

    add ID 8 to Litime BMS
    ECTIVE BM-X (monitor)

--- tests/bms/test_lipower_bms.py
+++ tests/bms/test_lipower_bms.py
@@ -22,6 +22,9 @@ _PROTO_DEFS: Final[dict[int, bytearray]] = {
     0x0B: bytearray(
         b"\x0b\x03\x12\x00\x70\x00\x5e\x00\x35\x00\x2d\x00\x01\x00\xc9\x00\x85\x00\x1a\x00\x02\x47\x16"
     ),  # 13.3V, 94%, 193500s, -2.01A, -26W, protocol version 0x02
+    0x08: bytearray(
+        b"\x08\x03\x10\xff\x32\x4f\xbf\x27\x10\x00\x00\x00\x01\x00\x08\x00\x8a\x00\x01\x5d\x4f"
+    ),
 }
 
 _RESULT_DEFS: Final[dict[int, BMSSample]] = {
@@ -47,12 +50,23 @@ _RESULT_DEFS: Final[dict[int, BMSSample]] = {
         "battery_charging": False,
         "problem": False,
     },
+    0x08: {
+        "voltage": 13.8,
+        "current": -0.08,
+        "battery_level": 94,
+        "cycle_charge": 112,
+        "cycle_capacity": 1489.6,
+        "power": -1.104,
+        "runtime": 36000000,
+        "battery_charging": False,
+        "problem": False,
+    },
 }
 
 
 @pytest.fixture(
     name="protocol_type",
-    params=[0x22, 0x0B],
+    params=[0x22, 0x0B, 0x08],
 )
 def proto(request: pytest.FixtureRequest) -> int:
     """Protocol fixture."""
