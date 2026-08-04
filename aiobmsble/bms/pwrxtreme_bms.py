e9977d5069a65398256fc26f33da10538ab896bb
Patrick <14628713+patman15@users.noreply.github.com>
2026-08-04 21:38:55 +0200

    add polling commands

--- aiobmsble/bms/pwrxtreme_bms.py
+++ aiobmsble/bms/pwrxtreme_bms.py
@@ -4,9 +4,11 @@ Project: aiobmsble, https://pypi.org/p/aiobmsble/
 License: Apache-2.0, http://www.apache.org/licenses/
 """
 
+import asyncio
+
 from bleak.uuids import normalize_uuid_str
 
-from aiobmsble import BMSInfo, MatcherPattern
+from aiobmsble import BMSInfo, BMSSample, MatcherPattern
 from aiobmsble.bms.topband_bms import BMS as TopbandBMS
 
 
@@ -43,3 +45,18 @@ class BMS(TopbandBMS):
     def uuid_tx() -> str:
         """Return 16-bit UUID of characteristic that provides write property."""
         return "fff2"
+
+    async def _async_update(self) -> BMSSample:
+        """Update battery status information."""
+
+        for cmd in (b"<B:AN>", b"<I:EM>", b"<I:WA>", b"<N:NA>"):
+            try:
+                await self._await_msg(cmd)
+            except TimeoutError:
+                continue
+            await asyncio.sleep(0.5)
+        return self._decode_data(BMS._FIELDS, self._msg, byteorder="little") | {
+            "cell_voltages": BMS._cell_voltages(
+                self._msg, cells=BMS._MAX_CELLS, start=22, byteorder="little"
+            )
+        }
