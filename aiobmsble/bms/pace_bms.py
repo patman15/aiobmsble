6b2d78bde686daa0bc44ff5e61931878a57a5613
Patrick <14628713+patman15@users.noreply.github.com>
2026-09-08 19:00:49 +0200

    Revert "add LVTOPSUN pattern for Pace BMS"
    This reverts commit 1d164e6f5bda7805f69984152ae645d622b86e82.

--- aiobmsble/bms/pace_bms.py
+++ aiobmsble/bms/pace_bms.py
@@ -55,10 +55,7 @@ class BMS(BaseBMS):
     @staticmethod
     def matcher_dict_list() -> list[MatcherPattern]:
         """Provide BluetoothMatcher definition."""
-        return [
-            {"local_name": pattern, "connectable": True}
-            for pattern in ("PC-????", "LLM_*")
-        ]
+        return [{"local_name": "PC-????", "connectable": True}]
 
     @staticmethod
     def uuid_services() -> tuple[str, ...]:
