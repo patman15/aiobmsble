9ec38549512a757c1f97b54f21c2b1381507268f
Patrick <14628713+patman15@users.noreply.github.com>
2026-09-13 15:35:50 +0200

    clean protocol definitions

--- aiobmsble/bms/daly_bms.py
+++ aiobmsble/bms/daly_bms.py
@@ -4,6 +4,10 @@ Project: aiobmsble, https://pypi.org/p/aiobmsble/
 License: Apache-2.0, http://www.apache.org/licenses/
 """
 
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import IntEnum
 from typing import Final, NamedTuple
 
 from bleak.backends.characteristic import BleakGATTCharacteristic
@@ -14,6 +18,13 @@ from aiobmsble import BMSConfig, BMSDp, BMSInfo, BMSSample, MatcherPattern, Temp
 from aiobmsble.basebms import BaseBMS, b2str, crc_modbus
 
 
+class Proto(IntEnum):
+    """Supported Daly protocol variants."""
+
+    D2 = 0xD2
+    X81 = 0x81
+
+
 class BMS(BaseBMS):
     """Daly smart BMS class implementation."""
 
@@ -21,7 +32,6 @@ class BMS(BaseBMS):
     _FCT_RD: Final[int] = 0x03
     _HEAD_LEN: Final[int] = 3
     _CRC_LEN: Final[int] = 2
-    _MAX_CELLS: Final[dict[int, int]] = {0xD2: 32, 0x81: 48}
     _MAX_TEMP: Final[int] = 8
     _MOSTEMP_POS: Final[int] = _HEAD_LEN + 8
 
@@ -30,52 +40,59 @@ class BMS(BaseBMS):
         adr: int
         length: int
 
-    _CMDS: Final[dict[int, dict[str, _dcmd]]] = {
-        0xD2: {
-            "resp": _dcmd(0xD2, 0, 0),
-            "rt1": _dcmd(_FCT_RD, 0x0, 62),
-            "mos": _dcmd(_FCT_RD, 0x3E, 0x9),
-            "info": _dcmd(_FCT_RD, 0xA9, 32),
-        },
-        0x81: {
-            "resp": _dcmd(0x51, 0, 0),
-            "rt1": _dcmd(_FCT_RD, 0x0, 64),
-            "rt2": _dcmd(_FCT_RD, 0x41, 62),
-            "info": _dcmd(_FCT_RD, 0x178, 74),
-        },
-    }
-    _FIELDS: Final[dict[int, tuple[BMSDp, ...]]] = {
-        0xD2: (
-            BMSDp("voltage", 80, 2, False, lambda x: x / 10, 62),
-            BMSDp("current", 82, 2, False, lambda x: (x - 30000) / 10, 62),
-            BMSDp("battery_level", 84, 2, False, lambda x: x / 10, 62),
-            BMSDp("cycle_charge", 96, 2, False, lambda x: x / 10, 62),
-            BMSDp(
-                "cell_count", 98, 2, False, lambda x: min(x, BMS._MAX_CELLS[0xD2]), 62
+    @dataclass(frozen=True, slots=True)
+    class _ProtoDef:
+        """Definition of a Daly protocol variant."""
+
+        resp: int
+        max_cells: int
+        cmds: tuple[BMS._dcmd, ...]
+        info: BMS._dcmd
+        mos: BMS._dcmd | None
+        fields: tuple[BMSDp, ...]
+
+    _PROTOS: Final[dict[Proto, BMS._ProtoDef]] = {
+        Proto.D2: _ProtoDef(
+            resp=0xD2,
+            max_cells=32,
+            cmds=(_dcmd(_FCT_RD, 0x0, 62),),
+            info=_dcmd(_FCT_RD, 0xA9, 32),
+            mos=_dcmd(_FCT_RD, 0x3E, 0x9),
+            fields=(
+                BMSDp("voltage", 80, 2, False, lambda x: x / 10, 62),
+                BMSDp("current", 82, 2, False, lambda x: (x - 30000) / 10, 62),
+                BMSDp("battery_level", 84, 2, False, lambda x: x / 10, 62),
+                BMSDp("cycle_charge", 96, 2, False, lambda x: x / 10, 62),
+                BMSDp("cell_count", 98, 2, False, lambda x: min(x, 32), 62),
+                BMSDp("temp_sensors", 100, 2, False, lambda x: min(x, 8), 62),
+                BMSDp("cycles", 102, 2, False, idx=62),
+                BMSDp("delta_voltage", 112, 2, False, lambda x: x / 1000, 62),
+                BMSDp("problem_code", 116, 8, False, lambda x: x % 2**64, 62),
+                BMSDp("balancer", 104, 2, False, idx=62),
+                BMSDp("chrg_mosfet", 106, 2, False, bool, 62),
+                BMSDp("dischrg_mosfet", 108, 2, False, bool, 62),
             ),
-            BMSDp("temp_sensors", 100, 2, False, lambda x: min(x, BMS._MAX_TEMP), 62),
-            BMSDp("cycles", 102, 2, False, idx=62),
-            BMSDp("delta_voltage", 112, 2, False, lambda x: x / 1000, 62),
-            BMSDp("problem_code", 116, 8, False, lambda x: x % 2**64, 62),
-            BMSDp("balancer", 104, 2, False, idx=62),
-            BMSDp("chrg_mosfet", 106, 2, False, bool, 62),
-            BMSDp("dischrg_mosfet", 108, 2, False, bool, 62),
         ),
-        0x81: (
-            BMSDp("voltage", 112, 2, False, lambda x: x / 10, 64),
-            BMSDp("current", 114, 2, False, lambda x: (x - 30000) / 10, 64),
-            BMSDp("battery_level", 116, 2, False, lambda x: x / 10, 64),
-            BMSDp(
-                "cell_count", 120, 2, False, lambda x: min(x, BMS._MAX_CELLS[0x81]), 64
+        Proto.X81: _ProtoDef(
+            resp=0x51,
+            max_cells=48,
+            cmds=(_dcmd(_FCT_RD, 0x0, 64), _dcmd(_FCT_RD, 0x41, 62)),
+            info=_dcmd(_FCT_RD, 0x178, 74),
+            mos=None,
+            fields=(
+                BMSDp("voltage", 112, 2, False, lambda x: x / 10, 64),
+                BMSDp("current", 114, 2, False, lambda x: (x - 30000) / 10, 64),
+                BMSDp("battery_level", 116, 2, False, lambda x: x / 10, 64),
+                BMSDp("cell_count", 120, 2, False, lambda x: min(x, 48), 64),
+                BMSDp("cycle_charge", 20, 2, False, lambda x: x / 10, 62),
+                BMSDp("temp_sensors", 122, 2, False, lambda x: min(x, 8), 64),
+                BMSDp("cycles", 22, 2, False, idx=62),
+                BMSDp("problem_code", 88, 4, False, idx=62),
+                BMSDp("balancer", 24, 2, False, idx=62),
+                BMSDp("chrg_mosfet", 34, 2, False, bool, 62),
+                BMSDp("dischrg_mosfet", 36, 2, False, bool, 62),
+                BMSDp("heater", 40, 2, False, bool, 62),
             ),
-            BMSDp("cycle_charge", 20, 2, False, lambda x: x / 10, 62),
-            BMSDp("temp_sensors", 122, 2, False, lambda x: min(x, BMS._MAX_TEMP), 64),
-            BMSDp("cycles", 22, 2, False, idx=62),
-            BMSDp("problem_code", 88, 4, False, idx=62),
-            BMSDp("balancer", 24, 2, False, idx=62),
-            BMSDp("chrg_mosfet", 34, 2, False, bool, 62),
-            BMSDp("dischrg_mosfet", 36, 2, False, bool, 62),
-            BMSDp("heater", 40, 2, False, bool, 62),
         ),
     }
 
@@ -87,8 +104,7 @@ class BMS(BaseBMS):
     ) -> None:
         """Initialize private BMS members."""
         super().__init__(ble_device, config, logger_name)
-        self._proto: int = 0xD2
-        self._rt_cmds: tuple[BMS._dcmd, ...] = ()
+        self._proto: Proto = Proto.D2
         self._msg: bytes = b""
         self._mos_avail: bool | None = None
 
@@ -106,7 +122,7 @@ class BMS(BaseBMS):
                 manufacturer_id=m_id,
                 connectable=True,
             )
-            for m_id in (0x102, 0x104, 0x0302, 0x0303, 0x0402)
+            for m_id in (0x102, 0x104, 0x302, 0x303, 0x402)
         ]
 
     @staticmethod
@@ -127,9 +143,9 @@ class BMS(BaseBMS):
     async def _fetch_device_info(self) -> BMSInfo:
         """Fetch the device information via BLE."""
         await self._await_msg(
-            BMS._cmd_modbus(self._proto, *BMS._CMDS[self._proto]["info"])
+            BMS._cmd_modbus(self._proto, *BMS._PROTOS[self._proto].info)
         )
-        if self._msg[0] == BMS._CMDS[0x81]["resp"][0]:
+        if self._msg[0] == BMS._PROTOS[Proto.X81].resp:
             return {
                 "sw_version": b2str(self._msg[3:31]),
                 "hw_version": b2str(self._msg[31:45]),
@@ -145,24 +161,19 @@ class BMS(BaseBMS):
         self, char_notify: BleakGATTCharacteristic | int | str | None = None
     ) -> None:
         await super()._init_connection(char_notify)
-        for self._proto in BMS._CMDS:
+        for self._proto in BMS._PROTOS:
             try:
                 await self._await_msg(
-                    BMS._cmd_modbus(self._proto, *BMS._CMDS[self._proto]["rt1"])
+                    BMS._cmd_modbus(self._proto, *BMS._PROTOS[self._proto].cmds[0])
                 )
                 self._log.debug("detected protocol: 0x%X", self._proto)
-                if "mos" not in BMS._CMDS[self._proto]:
+                if BMS._PROTOS[self._proto].mos is None:
                     self._mos_avail = False
-                self._rt_cmds = tuple(
-                    cmd
-                    for name, cmd in BMS._CMDS[self._proto].items()
-                    if name.startswith("rt")
-                )
                 break
             except TimeoutError:
                 ...  # try next protocol
         else:
-            self._proto = next(iter(BMS._CMDS))  # fallback to first protocol
+            self._proto = Proto.D2  # fallback to first protocol
             raise TimeoutError
 
     def _notification_handler(
@@ -172,7 +183,7 @@ class BMS(BaseBMS):
 
         if (
             len(data) < BMS._HEAD_LEN
-            or data[0] != BMS._CMDS[self._proto]["resp"][0]
+            or data[0] != BMS._PROTOS[self._proto].resp
             or data[1] != BMS._FCT_RD
             or data[2] != len(data) - BMS._HEAD_LEN - BMS._CRC_LEN
         ):
@@ -194,12 +205,11 @@ class BMS(BaseBMS):
     async def _async_update(self) -> BMSSample:
         """Update battery status information."""
         result: BMSSample = {}
-        if self._mos_avail in (True, None):
+        proto_def = BMS._PROTOS[self._proto]
+        if self._mos_avail in (True, None) and proto_def.mos is not None:
             try:
                 # request MOS temperature (possible: response, stuck response, no response)
-                await self._await_msg(
-                    BMS._cmd_modbus(self._proto, *BMS._CMDS[self._proto]["mos"])
-                )
+                await self._await_msg(BMS._cmd_modbus(self._proto, *proto_def.mos))
 
                 if self._mos_avail is None and self._msg[
                     BMS._MOSTEMP_POS : BMS._MOSTEMP_POS + 2
@@ -223,19 +233,17 @@ class BMS(BaseBMS):
                 self._mos_avail = False
 
         rt_msgs: dict[int, bytes] = {}
-        for cmd in self._rt_cmds:
+        for cmd in proto_def.cmds:
             await self._await_msg(BMS._cmd_modbus(self._proto, *cmd))
             if self._msg[2] // 2 != cmd.length:
                 self._log.debug("incorrect response %i", self._msg[2])
                 break
             rt_msgs[cmd.length] = self._msg
 
-        if len(rt_msgs) != len(self._rt_cmds):
+        if len(rt_msgs) != len(proto_def.cmds):
             raise ValueError("BMS data incomplete.")
 
-        result |= BMS._decode_data(
-            BMS._FIELDS[self._proto], rt_msgs, start=BMS._HEAD_LEN
-        )
+        result |= BMS._decode_data(proto_def.fields, rt_msgs, start=BMS._HEAD_LEN)
 
         rt1_msg: Final[bytes] = next(iter(rt_msgs.values()))
         # add temperature sensors
@@ -243,7 +251,7 @@ class BMS(BaseBMS):
             BMS._temp_values(
                 rt1_msg,
                 values=result.get("temp_sensors", 0),
-                start=BMS._MAX_CELLS[self._proto] * 2 + BMS._HEAD_LEN,
+                start=proto_def.max_cells * 2 + BMS._HEAD_LEN,
                 offset=40,
             )
         )
